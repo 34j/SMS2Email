7d7eb3741de08dc0df65931149d5f43f4452a9ce
34j <34j.github@proton.me>
2025-12-28 17:43:44 +0900

    chore(release): fix version bumped after build

--- release.config.js
+++ release.config.js
@@ -9,7 +9,7 @@ module.exports = {
         // Increment versionCode, update versionName, write changelog entry, and build signed APK
         prepareCmd: `
         sed -i -E "s/versionCode = [0-9]+/versionCode = $(( $(grep -oP 'versionCode = \\K[0-9]+' app/build.gradle.kts) + 1 ))/" app/build.gradle.kts
-        sed -i -E "s/versionName = \".*\"/versionName = \"\${nextRelease.version}\"/" app/build.gradle.kts
+        sed -i -E 's/versionName = .*/versionName = "'"'"${nextRelease.version}"'"'"/' app/build.gradle.kts
         mkdir -p metadata/en-US/changelogs
         echo "\${nextRelease.notes}" > metadata/en-US/changelogs/$(grep -oP 'versionCode = \\K[0-9]+' app/build.gradle.kts).txt
         ./gradlew :app:assembleRelease \
